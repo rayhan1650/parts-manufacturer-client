6090c6d2fe0a5c1d1fba0db2753736b1404818f4
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-22 23:04:15 +0600

    not found page added

--- src/Pages/Shared/PageNotFound/PageNotFound.js
+++ src/Pages/Shared/PageNotFound/PageNotFound.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./PageNotFound.css";
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h2>PageNotFound</h2>
+    <div id="not-found">
+      <div className="not-found">
+        <div className="relative">
+          <img
+            className="animate-pulse px-10 mx-auto"
+            src="https://i.ibb.co/nzBpK9k/NotFound.jpg"
+            alt=""
+          />
+        </div>
+
+        <div className="not-found-404">
+          <h1>404</h1>
+          <h2 className="animate-bounce">Page not found</h2>
+        </div>
+        <Link to="/">Homepage</Link>
+      </div>
     </div>
   );
 };
