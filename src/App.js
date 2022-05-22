25a76653723b9e6cef0f12139cc9e84a34ac667b
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-22 22:05:54 +0600

    react router installed & some header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blogs from "./Pages/Blogs/Blogs";
+import Home from "./Pages/Home/Home/Home";
+import Header from "./Pages/Shared/Header/Header";
+import PageNotFound from "./Pages/Shared/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div>
-      <h2>Hello World</h2>
-      <button className="btn btn-primary">Hello</button>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        {/* for dropdown  */}
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+
+        {/* website logo  */}
+        <Link to="/home" className="btn btn-ghost normal-case text-xl">
+          Manufacturer
+        </Link>
+      </div>
+
+      {/* for main navigation items  */}
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* for end nav  */}
+      <div className="navbar-end">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <Link to="/home">Login</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
