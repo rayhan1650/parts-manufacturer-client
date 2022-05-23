a94901631a9b5b9b90853e77440569706a61bc28
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-23 14:40:35 +0600

    loading page added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Loading from "../../Shared/Loading/Loading";
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <Loading />
     </div>
   );
 };
