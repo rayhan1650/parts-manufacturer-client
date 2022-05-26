9a955fb7f4f499a0d2498becbf04f2a5c15ce05b
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-26 07:18:23 +0600

    purchase data added

--- src/Pages/Home/Purchase/Purchase.js
+++ src/Pages/Home/Purchase/Purchase.js
@@ -11,8 +11,24 @@ const Purchase = () => {
       .then((data) => setParts(data));
   }, []);
   return (
-    <div>
-      <h2>{parts.name}</h2>
+    <div className="px-4 lg:px-14 py-6 ">
+      <div className="hero min-h-screen  bg-sky-100 shadow-xl rounded">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={parts.img} alt="" />
+          <div>
+            <h1 className="text-5xl font-bold text-primary">{parts.name}</h1>
+            <p className="py-6 text-justify">{parts.description}</p>
+            <h4 className="font-bold">
+              Available Quantity: {parts.availableQuantity}
+            </h4>
+            <h4 className="font-bold">
+              Minimum Order Quantity: {parts.minOrderQuantity}
+            </h4>
+            <h4 className="font-bold">Price Per-Unit: {parts.pricePerUnit}</h4>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
