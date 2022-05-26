f57f1dce27f90981be5c1135e933a62a1fca20e3
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-26 17:24:00 +0600

    simple review added and parts reverse implemented

--- src/Pages/Home/Parts/Parts.js
+++ src/Pages/Home/Parts/Parts.js
@@ -8,9 +8,10 @@ const Parts = () => {
       .then((res) => res.json())
       .then((data) => setParts(data));
   }, []);
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 px-2 py-6 lg:px-14">
-      {parts.map((part) => (
+      {parts.slice(0, 6).map((part) => (
         <Part key={part._id} part={part} />
       ))}
     </div>
---
+++ src/Pages/Home/Reviews/Reviews.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Review from "../Review/Review";
+
+const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/reviews")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <div className="px-2 py-6 lg:px-14">
+      <h2 className="text-center font-bold text-4xl text-primary mb-6">
+        Reviews
+      </h2>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 ">
+        {reviews.map((review) => (
+          <Review key={review._id} review={review} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
